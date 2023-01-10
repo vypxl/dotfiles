249bd92b7dbaa564b013240837725336419ef38f
vypxl <thomas@vypxl.io>
2023-01-10 19:20:57 +0100

    fish format

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,11 @@ set -gx PATH $PATH $HOME/.dotnet/tools
 set -gx PATH $PATH $HOME/.ghcup/bin
 set -gx PNPM_HOME "/home/thomas/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
-set -gx PATH $PATH $HOME/.local/bin 
+set -gx PATH $PATH $HOME/.local/bin
 set -gx PATH $PATH $HOME/.nimble/bin
 if type -q ruby
-  set -gx PATH $PATH (ruby -r rubygems -e 'puts Gem.dir+ "/bin"')
-  set -gx PATH $PATH (ruby -r rubygems -e 'puts Gem.user_dir+ "/bin"')
+    set -gx PATH $PATH (ruby -r rubygems -e 'puts Gem.dir+ "/bin"')
+    set -gx PATH $PATH (ruby -r rubygems -e 'puts Gem.user_dir+ "/bin"')
 end
 set -gx PATH $PATH $HOME/.cargo/bin
 set -gx PATH $PATH /usr/lib/emscripten
@@ -16,7 +16,7 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx SXHKD_SHELL /bin/sh
 set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/init.py
 set -gx DEBUGINFOD_URLS (cat "/etc/debuginfod"/*.urls 2> /dev/null | tr '\n' ' ')
-set -gx EDITOR "vim"
+set -gx EDITOR vim
 set -gx XMODIFIERS "@im=fcitx"
 set -gx XMODIFIER "@im=fcitx"
 set -gx GTK_IM_MODULE fcitx
@@ -25,12 +25,12 @@ set -gx DefaultIMModule fcitx
 
 # Prompt
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
 
 # Completions
 if type -q gh
-  gh completion --shell fish | source
+    gh completion --shell fish | source
 end
 
 # fzf, fd, rg and friends
@@ -42,40 +42,44 @@ alias zp="fzf --height=90% --preview-window=down:80% --preview='bat -n --color=a
 alias zpg="fzf --height=90% --preview-window=down:80% --preview='bat -n --color=always -r=:500 (echo {} | cut -d: -f1)'"
 
 function vz
-  set f (zp)
-  test -n "$f" && vim "$f"
+    set f (zp)
+    test -n "$f" && vim "$f"
 end
 
 function vgz
-  if test (count $argv) -lt 1; set argv ""; end
-  set f (rg $argv | zpg | cut -d: -f1)
-  test -n "$f" && vim "$f"
+    if test (count $argv) -lt 1
+        set argv ""
+    end
+    set f (rg $argv | zpg | cut -d: -f1)
+    test -n "$f" && vim "$f"
 end
 
 function cz
-  set d (fd -Htd | fzf)
-  test -n "$d" && cd "$d"
+    set d (fd -Htd | fzf)
+    test -n "$d" && cd "$d"
 end
 
 function cgz
-  if test (count $argv) -lt 1; set argv ""; end
-  set d (rg $argv | zpg | cut -d: -f1 | xargs dirname)
-  test -n "$d" && cd "$d"
+    if test (count $argv) -lt 1
+        set argv ""
+    end
+    set d (rg $argv | zpg | cut -d: -f1 | xargs dirname)
+    test -n "$d" && cd "$d"
 end
 
 # Convenience Functions
 function cdd
-  command mkdir -p $argv
-  cd $argv
+    command mkdir -p $argv
+    cd $argv
 end
 
 function fork
-  if test (count $argv) -lt 1
-    # echo "Please specify command to fork!!"
-    fork kitty
-  else
-    ruby -e "require 'open3'; fork do; Open3.capture3(ARGV.join('')); end" $argv
-  end
+    if test (count $argv) -lt 1
+        # echo "Please specify command to fork!!"
+        fork kitty
+    else
+        ruby -e "require 'open3'; fork do; Open3.capture3(ARGV.join('')); end" $argv
+    end
 end
 
 function join
@@ -83,35 +87,35 @@ function join
 end
 
 function _latest_command
-  commandline -r $history[1]
-  commandline -f execute
+    commandline -r $history[1]
+    commandline -f execute
 end
 bind \cr _latest_command
 
 function pls
-  if test (count $argv) -lt 1
-    commandline -r "sudo $history[1]"
-    commandline -f execute
-  else
-    sudo $argv
-  end
+    if test (count $argv) -lt 1
+        commandline -r "sudo $history[1]"
+        commandline -f execute
+    else
+        sudo $argv
+    end
 end
 
 function reload
-  source ~/.config/fish/config.fish
+    source ~/.config/fish/config.fish
 end
 
 function update
-  echo "==> Updating repository and AUR <=="
-  paru -Syu
-  echo "==> Updating ruby gems <=="
-  gem update (gem outdated | cut -d ' ' -f 1)
-  echo "==> Updating npm packages <=="
-  pnpm update -g
-  echo "==> Updating pip packages <=="
-  pip list --format=freeze --user | cut -d = -f 1 \
-    | xargs pip install --user --upgrade | grep -v "already"
-  true
+    echo "==> Updating repository and AUR <=="
+    paru -Syu
+    echo "==> Updating ruby gems <=="
+    gem update (gem outdated | cut -d ' ' -f 1)
+    echo "==> Updating npm packages <=="
+    pnpm update -g
+    echo "==> Updating pip packages <=="
+    pip list --format=freeze --user | cut -d = -f 1 \
+        | xargs pip install --user --upgrade | grep -v already
+    true
 end
 
 # Convenience Aliases
@@ -121,21 +125,23 @@ alias la="ls -al"
 alias lr="la -R"
 alias .=ll
 
-alias vim=nvim
+alias vim=/usr/bin/nvim
+alias nvim='/usr/bin/nvim "+lua require(\'persistence\').load()"'
+alias hx=helix
 alias cclip="xclip -selection clipboard"
 alias beep="aplay -q ~/.config/misc/beep.wav"
 alias pluto="julia -ie 'import Pluto; Pluto.run()'"
 alias icat="kitty +kitten icat"
 
 abbr -a md mkdir
-abbr -a g   git
+abbr -a g git
 abbr -a ga. git add .
-abbr -a ga  git add
+abbr -a ga git add
 abbr -a gst git status
 abbr -a gcm git commit -m
 abbr -a gcam git commit -am
-abbr -a gc  git checkout
-abbr -a gp  git push --all
+abbr -a gc git checkout
+abbr -a gp git push --all
 abbr -a gpl git pull
 
 abbr -a pn pnpm
@@ -155,4 +161,4 @@ end
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and source ~/.config/tabtab/fish/__tabtab.fish; or true
 
 # opam configuration
-source /home/thomas/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source /home/thomas/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true

d81b97ab3079be30ebc0666234e84655bb47beda
vypxl <thomas@vypxl.io>
2023-01-17 12:09:50 +0100

    Packages: use neovim nightly

--- .bootstrap/packages.rb
+++ .bootstrap/packages.rb
@@ -1,6 +1,6 @@
 require './util'
 
-PACKAGES = %w(
+PACKAGES = %w[
   base-devel
   busybox
   inetutils
@@ -21,7 +21,7 @@ PACKAGES = %w(
   syncthingtray
 
   vim
-  neovim
+  neovim-nigthly-bin
   nvim-packer-git
   fd
   jq
@@ -91,17 +91,17 @@ PACKAGES = %w(
   sdl2
   sfml
   freetype2
-)
+]
 
-RUBY_PACKAGES = %w(
+RUBY_PACKAGES = %w[
   neovim
   rake
   rails
   solargraph
   terutil
-)
+]
 
-NPM_PACKAGES = %w(
+NPM_PACKAGES = %w[
   firebase-tools
   live-server
   n
@@ -112,9 +112,9 @@ NPM_PACKAGES = %w(
   graphql-language-service-cli
   typescript
   ls_emmet
-)
+]
 
-PYTHON_PACKAGES = %w(
+PYTHON_PACKAGES = %w[
   conan
   neovim
   networkx
@@ -122,48 +122,48 @@ PYTHON_PACKAGES = %w(
   pyright
   pwntools
   toolz
-)
+]
 
 # TODO!!!
 def install_rust
   # if not installed:
-  log_exec_fail "sudo rustup install nightly"
+  log_exec_fail 'sudo rustup install nightly'
 end
 
 def install_node
   `bash -c "command -v n"`
   return if $?.success?
 
-  puts "Installing NodeJS"
-  log_exec_fail "curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /tmp/n"
-  log_exec_fail "sudo bash /tmp/n lts"
-  log_exec_fail "npx --yes pnpm i -g pnpm"
-  log_exec_fail "rm /tmp/n"
+  puts 'Installing NodeJS'
+  log_exec_fail 'curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /tmp/n'
+  log_exec_fail 'sudo bash /tmp/n lts'
+  log_exec_fail 'npx --yes pnpm i -g pnpm'
+  log_exec_fail 'rm /tmp/n'
 end
 
 def install_system_packages
   pkgs = PACKAGES.join ' '
-  puts "Installing System Packages"
+  puts 'Installing System Packages'
   log_exec_fail "paru -S --needed --noconfirm #{pkgs}"
 end
 
 def install_ruby_packages
   pkgs = RUBY_PACKAGES.join ' '
-  puts "Installing Ruby gems"
+  puts 'Installing Ruby gems'
   log_exec_fail "gem install --conservative #{pkgs}"
 end
 
 def install_python_packages
   pkgs = PYTHON_PACKAGES.join ' '
-  puts "Installing Python (and PyPy) packages"
+  puts 'Installing Python (and PyPy) packages'
   log_exec_fail "pip install --user #{pkgs}"
-  log_exec_fail "pypy3 -m ensurepip --user"
-  log_exec_fail "pypy3 -mpip install --user #{pkgs}" 
+  log_exec_fail 'pypy3 -m ensurepip --user'
+  log_exec_fail "pypy3 -mpip install --user #{pkgs}"
 end
 
 def install_npm_packages
   pkgs = NPM_PACKAGES.join ' '
-  puts "Installing global npm packages"
+  puts 'Installing global npm packages'
   log_exec_fail "pnpm i -g #{pkgs}"
 end
 
@@ -175,4 +175,3 @@ def install_packages
   install_npm_packages
   install_python_packages
 end
-

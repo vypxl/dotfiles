20d56e35648a97374a66d58b9f436309ffd9a8da
vypxl <thomas@vypxl.io>
2024-01-09 23:17:17 +0900

    fish: add chxdg function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -102,6 +102,26 @@ function x
     echo $argv | jq -s 'reduce .[] as $item (1; . * $item)'
 end
 
+function chxdg
+    if test (count $argv) -lt 1
+        chxdg -h
+    else if test (count $argv) = 1
+        if test $argv[1] = -h
+            echo "Usage: chxdg <basedir> [cmd]"
+        else
+            chxdg $argv[1] fish
+        end
+    else if test (count $argv) = 2
+        set base $argv[1]
+        set -lx XDG_CONFIG_HOME $base/.config
+        set -lx XDG_CACHE_HOME $base/.cache
+        set -lx XDG_DATA_HOME $base/.local/share
+        $argv[2]
+    else
+        chxdg -h
+    end
+end
+
 function _latest_command
     commandline -r $history[1]
     commandline -f execute

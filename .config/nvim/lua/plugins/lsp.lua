084dcfc2cb17af02b9eab9241ba4662b1e20fdec
vypxl <thomas@vypxl.io>
2024-04-11 10:21:48 +0200

    nvim: remove deprecated none-ls sources

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -127,11 +127,8 @@ local function setup_null_ls()
   local sources = {
     fmt.alejandra,
     fmt.asmfmt,
-    fmt.beautysh,
     fmt.black,
-    fmt.brittany, -- Haskell
     fmt.buildifier,
-    fmt.cabal_fmt,
     fmt.cbfmt,
     fmt.clang_format,
     fmt.cljstyle,
@@ -139,16 +136,13 @@ local function setup_null_ls()
     fmt.crystal_format.with { extra_args = { "-" } },
     fmt.dfmt,
     fmt.fish_indent,
-    fmt.fixjson,
     fmt.fnlfmt,
     fmt.gdformat,
     fmt.gofmt,
     fmt.goimports,
     fmt.google_java_format,
     fmt.isort, -- Python
-    fmt.jq, -- JSON
     fmt.ktlint,
-    fmt.latexindent,
     fmt.markdownlint,
     fmt.mix, -- Elixir
     fmt.nginx_beautifier,
@@ -160,19 +154,15 @@ local function setup_null_ls()
     fmt.protolint,
     fmt.ptop,
     fmt.rubocop,
-    -- fmt.rustfmt.with { extra_args = { "--edition=2021" } },
     fmt.scalafmt,
     fmt.stylua.with { extra_args = { "--indent-type", "Spaces", "--indent-width", "2" } },
     fmt.surface, -- Phoenix
     fmt.swiftformat,
-    fmt.taplo,
     fmt.yamlfmt,
-    fmt.zigfmt,
 
     diag.buildifier,
     diag.checkmake,
     diag.checkstyle,
-    diag.chktex,
     diag.clj_kondo,
     diag.cmake_lint,
     diag.credo,
@@ -180,22 +170,16 @@ local function setup_null_ls()
     diag.fish,
     diag.gdlint,
     diag.hadolint,
-    diag.jsonlint,
     diag.ktlint,
-    diag.misspell,
     diag.protolint,
-    diag.pydocstyle,
     diag.pylint,
     diag.revive,
     diag.selene,
-    diag.shellcheck,
     diag.statix,
     diag.swiftlint,
     diag.tidy,
-    diag.tsc,
     diag.yamllint,
 
-    ca.shellcheck,
     ca.statix,
   }
 

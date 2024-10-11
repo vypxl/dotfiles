349ac0d4a37b3d8c96f925d009017e056274eef0
vypxl <thomas@vypxl.io>
2024-10-11 12:00:40 +0200

    tmux env and cmd segments

--- .config/tmux-powerline/segments/vypxl-cmd.sh
+++ .config/tmux-powerline/segments/vypxl-cmd.sh
@@ -1,13 +1,12 @@
 run_segment() {
   tty=$(tmux display-message -p "#{pane_tty}")
-  pid=$(ps -o pid= -t "$tty" | tail -n 1)
-  cmdline=$(cat /proc/$pid/cmdline | tr '\0' ' ')
+  pid=$(ps -o pid= -t "$tty" | tail -n 1 | xargs echo)
+  cmdline=$(cat "/proc/$pid/cmdline" | tr '\0' ' ')
   cmd=$(echo $cmdline | cut -d' ' -f1)
 
-  echo -n $cmd
-  if echo "cargo docker make just go ssh" | grep -qw "$cmd"; then
+  if echo "cargo docker make just go" | grep -qw "$cmd"; then
     echo $(echo $cmdline | cut -d' ' -f-2)
-  elif ! echo "fish bash sh" | grep -qw "$cmd"; then
+  elif ! echo "fish bash sh ssh" | grep -qw "$cmd"; then
     echo $cmd
   fi
 }
--- .config/tmux-powerline/segments/vypxl-env.sh
+++ .config/tmux-powerline/segments/vypxl-env.sh
@@ -1,4 +1,9 @@
 run_segment() {
+  tty=$(tmux display-message -p "#{pane_tty}")
+  pid=$(ps -o pid= -t "$tty" | tail -n 1 | xargs echo)
+  cmdline=$(cat "/proc/$pid/cmdline" | tr '\0' ' ')
+  cmd=$(echo $cmdline | cut -d' ' -f1)
+
   if [[ -n "$IN_NIX_SHELL" ]]; then
     echo "nix"
   fi
@@ -7,11 +12,12 @@ run_segment() {
     echo "venv: $VIRTUAL_ENV"
   fi
 
-  if [ -f /.dockerenv ]; then
+  if echo $cmd | grep -qwe "^docker"; then
     echo "docker"
   fi
 
-  if [[ -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
-    echo $(whoami)@$(hostname)
+  if echo $cmd | grep -qwe "^ssh"; then
+    ssh_info=$(echo $cmdline | awk '{print $NF}')
+    echo "> $ssh_info"
   fi
 }

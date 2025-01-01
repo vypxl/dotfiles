7e92aeef1c0aff864387cd9b4fd56f87b26bda4e
vypxl <thomas@vypxl.io>
2025-01-01 18:20:05 +0100

    bluetooth

--- hosts/stone/configuration.nix
+++ hosts/stone/configuration.nix
@@ -5,6 +5,7 @@
     ../../nixos/modules/audio.nix
     ../../nixos/modules/auto-upgrade.nix
     ../../nixos/modules/base.nix
+    ../../nixos/modules/bluetooth.nix
     ../../nixos/modules/boot.nix
     ../../nixos/modules/docker.nix
     ../../nixos/modules/fish.nix
---
+++ nixos/modules/bluetooth.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+}

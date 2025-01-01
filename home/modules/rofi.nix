90b02439b380ddf4d976c999031c3dbe06351094
vypxl <thomas@vypxl.io>
2025-01-01 21:42:35 +0100

    remove rofi

--- home/modules/rofi.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-with import ../src/util.nix config;
-{
-  home.packages = [ pkgs.rofi ];
-  xdg.configFile."rofi" = dotfile_dir "rofi";
-}
--- home/thomas.nix
+++ home/thomas.nix
@@ -21,7 +21,6 @@ with import ./src/util.nix config;
     ./modules/lazygit.nix
     ./modules/nvim.nix
     ./modules/python.nix
-    ./modules/rofi.nix
     ./modules/shell-utils.nix
     ./modules/ssh.nix
     ./modules/syncthing.nix

2ecaf77945e1218f859abc6b7ea47c1fd8af7d64
vypxl <thomas@vypxl.io>
2025-01-03 14:04:52 +0100

    refactor flake

--- flake.nix
+++ flake.nix
@@ -24,36 +24,27 @@
           config.allowUnfree = true;
         };
       };
-      unstable-overlay =
-        { ... }:
-        {
-          nixpkgs.overlays = [ overlay-unstable ];
-        };
-      hm-opts =
-        { ... }:
-        {
-          home-manager.backupFileExtension = "hmbak";
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-        };
-      hm-user = username: {
-        home-manager.users."${username}" = import ./home/${username}.nix;
-      };
       machine =
         hostname: username:
         nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = attrs;
           modules = [
-            unstable-overlay
+            { nixpkgs.overlays = [ overlay-unstable ]; }
             home-manager.nixosModules.default
-            hm-opts
-            (hm-user username)
+            { 
+              home-manager.backupFileExtension = "hmbak";
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."${username}" = import ./home/${username}.nix;
+            }
             ./hosts/${hostname}/configuration.nix
           ];
         };
     in
     {
-      nixosConfigurations.stone = machine "stone" "thomas";
+      nixosConfigurations = {
+        stone = machine "stone" "thomas";
+      };
     };
 }

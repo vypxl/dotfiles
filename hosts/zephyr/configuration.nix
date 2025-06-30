38c31768b43e8644dc91e0beb49eebd1084887ea
vypxl <thomas@vypxl.io>
2025-06-30 09:01:06 +0200

    zephyr system

--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -7,6 +7,9 @@
     "${builtins.fetchGit { url = "https://github.com/cgrohs27/nixos-hardware.git"; rev = "64bb927d282e67f9b37be2b5c1bd8ce3b73178d2"; }}/asus/zephyrus/ga403"
   ];
 
+  boot.kernelPackages = pkgs.linuxPackages_6_13;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
   my.bundle.desktop.enable = true;
 
   my.boot.splash = true;

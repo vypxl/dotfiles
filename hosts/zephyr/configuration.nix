8122e39ee181496d08944810e206f3ff6fdbe60f
vypxl <thomas@vypxl.io>
2025-10-06 14:54:19 +0200

    zephyr: linux 616

--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -7,8 +7,15 @@
     "${builtins.fetchGit { url = "https://github.com/cgrohs27/nixos-hardware.git"; rev = "64bb927d282e67f9b37be2b5c1bd8ce3b73178d2"; }}/asus/zephyrus/ga403"
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_6_15;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  boot.kernelPackages = pkgs.linuxPackages_6_16;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.65.06";
+    sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
+    openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
+    settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
+    usePersistenced = false;
+  };
 
   my.bundle.desktop.enable = true;
 

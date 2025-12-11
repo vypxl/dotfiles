18152762c4b39dc996b853376c1c67c5f3af50ef
vypxl <thomas@vypxl.io>
2025-12-11 14:02:01 +0100

    swap

--- nixos/modules/swap.nix
+++ nixos/modules/swap.nix
@@ -8,13 +8,27 @@ in
   };
 
   config = lib.mkIf cfg.swap.enable {
-    zramSwap = {
-      enable = true;
-      memoryPercent = 33;
-    };
+    # zramSwap = {
+    #   enable = true;
+    #   memoryPercent = 33;
+    # };
+
+    swapDevices = [{
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+      options = [ "discard" ];
+    }];
 
     boot.kernel.sysctl = {
-      "vm.swappiness" = 50;
+      "vm.swappiness" = 30;
     };
+
+    boot.kernelParams = [
+      "zswap.enabled=1" # enables zswap
+      "zswap.compressor=zstd" # compression algorithm
+      "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+      "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+    ];
   };
 }
+
